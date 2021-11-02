d88248e9fc4a3f0f645c153e6e71df5af7b71230
jino <choijinho705@gmail.com>
2021-11-02 15:16:54 +0900

    State hoisting

--- week 1-Jetpack Compose basics/app/src/main/java/com/example/basicscodelabsample/MainActivity.kt
+++ week 1-Jetpack Compose basics/app/src/main/java/com/example/basicscodelabsample/MainActivity.kt
@@ -3,14 +3,10 @@ package com.example.basicscodelabsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -69,7 +65,7 @@ private fun Greeting(name: String) {
     }
 }
 
-@Preview(showBackground = true, name = "DefaultPreview", widthDp = 320)
+//@Preview(showBackground = true, name = "DefaultPreview", widthDp = 320)
 @Composable
 private fun DefaultPreview() {
     BasicsCodelabSampleTheme {
@@ -79,9 +75,54 @@ private fun DefaultPreview() {
 
 @Composable
 private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    var shouldShowOnboarding by remember {
+        mutableStateOf(true)
+    }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings(names)
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String>) {
     Column(modifier = Modifier.padding(all = 8.dp)) {
         for (name in names) {
             Greeting(name = name)
         }
+    }
+}
+
+@Composable
+private fun OnboardingScreen(onClicked: () -> Unit) {
+    // TODO: This state should be hoisted
+    /*var shouldShowOnboarding by remember {
+        mutableStateOf(true)
+    }*/
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+private fun OnboardingPreview() {
+    BasicsCodelabSampleTheme {
+        OnboardingScreen(onClicked = {})
     }
 }
