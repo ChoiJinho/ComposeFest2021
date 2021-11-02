9a1c4022818086f03164529c8f05e56145a7c6c6
jino <choijinho705@gmail.com>
2021-11-02 14:58:21 +0900

    State in Compose

--- week 1-Jetpack Compose basics/app/src/main/java/com/example/basicscodelabsample/MainActivity.kt
+++ week 1-Jetpack Compose basics/app/src/main/java/com/example/basicscodelabsample/MainActivity.kt
@@ -5,13 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,16 +31,36 @@ class MainActivity : ComponentActivity() {
 private fun Greeting(name: String) {
     Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp)) {
         Row(modifier = Modifier.padding(all = 24.dp)) {
+            var isExpanded by remember {
+                mutableStateOf(false)
+            }
+
+            try {
+                (isExpanded as State<*>).value
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            var expanded = remember {
+                mutableStateOf(false)
+            }
+
+            (expanded as State<*>).value
+
+
+            val extraPadding = if (expanded.value) 48.dp else 0.dp
+
             Column(
                 modifier = Modifier
                     .weight(weight = 1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
 
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(text = if (!expanded.value) "Show more" else "Show less")
             }
         }
 
