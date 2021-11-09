c51bae14e20a4d21b0c4f7adf5ad884396c7dd87
jino <choijinho705@gmail.com>
2021-11-09 11:21:45 +0900

    Modifiers

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/basicscodelabsample/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/basicscodelabsample/MainActivity.kt
@@ -3,11 +3,22 @@ package com.example.basicscodelabsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicscodelabsample.ui.theme.BasicsCodelabSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,10 +26,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabSampleTheme {
-                // A surface container using the 'background' color from the theme
+                /*// A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
-                }
+                }*/
+
+                PhotographerCard()
             }
         }
     }
@@ -29,10 +42,63 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabSampleTheme {
         Greeting("Android")
     }
 }
+
+@Composable
+fun PhotographerCard() {
+    Row(
+        modifier = Modifier
+            .padding(all = 48.dp)
+            .clip(shape = RoundedCornerShape(size = 16.dp))
+            .clickable(onClick = {})
+            .padding(all = 16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(alignment = CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            // LocalContentAlpha is defining opacity level of its children
+            CompositionLocalProvider(values = arrayOf(LocalContentAlpha provides ContentAlpha.medium)) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+const val Blue = 0xFF0000FF
+
+@Preview(showBackground = true, backgroundColor = Blue)
+@Composable
+fun PhotographerCardPreview() {
+    BasicsCodelabSampleTheme {
+        PhotographerCard()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
